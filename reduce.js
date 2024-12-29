03cadca5b7ed0748023b8bdfa4b60ad59249b6cb
hasanemam1717 <hasanemam1717@gmail.com>
2024-12-29 22:38:46 +0600

    added reduce

--- reduce.js
+++ reduce.js
@@ -1,7 +1,7 @@
 const numbers = [1, 2, 3, 3];
 
 const result = numbers.reduce((previousValue, currentValue, index, arr) => {
-  console.log(index, arr[index]);
+  //   console.log(index, arr[index]);
   //   console.log("Previous Value :", previousValue);
   //   console.log("Current Value :", currentValue);
   return previousValue + currentValue;
